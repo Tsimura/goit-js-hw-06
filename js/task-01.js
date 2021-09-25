21fd010be98b97e565178ff105e1d2c73cd6e522
Yurii Tsimura <chappa2828@gmail.com>
2021-09-25 16:26:11 +0300

    task - 2

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,5 @@
+// переробити костилі через map
+
 const itemEl = document.querySelector('#categories');
 
 const itemListEl = itemEl.querySelectorAll('.item');
@@ -5,13 +7,13 @@ console.log('Number of categories: ', itemListEl.length);
 
 const titleEl = itemEl.querySelectorAll('h2');
 
-const itemUlInUl = itemEl.querySelectorAll('ul');
+const itemListInUl = itemEl.querySelectorAll('ul');
 
 console.log('Category: ', titleEl[0].textContent);
-console.log('Elements: ', itemUlInUl[0].querySelectorAll('li').length);
+console.log('Elements: ', itemListInUl[0].querySelectorAll('li').length);
 
 console.log('Category: ', titleEl[1].textContent);
-console.log('Elements: ', itemUlInUl[1].querySelectorAll('li').length);
+console.log('Elements: ', itemListInUl[1].querySelectorAll('li').length);
 
 console.log('Category: ', titleEl[2].textContent);
-console.log('Elements: ', itemUlInUl[2].querySelectorAll('li').length);
+console.log('Elements: ', itemListInUl[2].querySelectorAll('li').length);
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,34 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// Варіант 1
+
+// const ingredientEl = [];
+
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const option = ingredients[i];
+//   const linkEl = document.createElement('li');
+
+//   linkEl.classList.add('item');
+//   linkEl.textContent = option;
+
+//   ingredientEl.push(linkEl);
+// }
+
+// const listEl = document.querySelector('#ingredients');
+// listEl.append(...ingredientEl);
+
+// Варіант 2
+
+const ingredientEl = ingredients.map(option => {
+  const linkEl = document.createElement('li');
+
+  linkEl.classList.add('item');
+  linkEl.textContent = option;
+  return linkEl;
+});
+// console.log(ingredientEl);
+
+const listEl = document.querySelector('#ingredients');
+listEl.append(...ingredientEl);
