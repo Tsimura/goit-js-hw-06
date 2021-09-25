1308e344d24a8de0f11bd9f2226a082b3fca7dd6
Tsimura <chappa2828@gmail.com>
2021-09-25 22:16:48 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,16 @@
-// переробити костилі через map
-// https://youtu.be/Haq2JIuE6Rg?t=6056
-
 const itemEl = document.querySelector('#categories');
 
 const itemListEl = itemEl.querySelectorAll('.item');
+
 console.log('Number of categories: ', itemListEl.length);
 
 const titleEl = itemEl.querySelectorAll('h2');
 
 const itemListInUl = itemEl.querySelectorAll('ul');
 
-console.log('Category: ', titleEl[0].textContent);
-console.log('Elements: ', itemListInUl[0].querySelectorAll('li').length);
-
-console.log('Category: ', titleEl[1].textContent);
-console.log('Elements: ', itemListInUl[1].querySelectorAll('li').length);
-
-console.log('Category: ', titleEl[2].textContent);
-console.log('Elements: ', itemListInUl[2].querySelectorAll('li').length);
+for (let i = 0; i < titleEl.length; i++) {
+  const category = titleEl[i].textContent;
+  const elements = itemListInUl[i].querySelectorAll('li').length;
+  console.log('Category: ', category);
+  console.log('Elements: ', elements);
+}
