7a23014a99341d12c955b2d8953b3ec1c757145a
Yurii Tsimura <chappa2828@gmail.com>
2021-09-28 12:48:10 +0300

    task - 6

---
+++ js/task-06.js
@@ -0,0 +1,24 @@
+const inputEl = document.querySelector('#validation-input');
+inputEl.addEventListener('focus', onInputFocus);
+inputEl.addEventListener('blur', onInputBlur);
+inputEl.addEventListener('change', onInputChange);
+
+function onInputFocus() {
+  console.log('input в стані "focus"');
+}
+
+function onInputBlur() {
+  console.log('input в стані "blur"');
+}
+
+function onInputChange(event) {
+  if (this.value.length == this.getAttribute('data-length')) {
+    this.classList.add('valid');
+    this.classList.remove('invalid');
+    console.log('Вірна умова! Продовжуємо.');
+  } else {
+    this.classList.add('invalid');
+    this.classList.remove('valid');
+    console.log('Не вірна умова! кількість символів 6.');
+  }
+}
