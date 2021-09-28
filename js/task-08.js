ad69c03c5c6aa8fe9814031dd632f13ba38f669b
Tsimura <chappa2828@gmail.com>
2021-09-28 22:03:25 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -13,13 +13,23 @@
 
 // Выведи обьект с введенными данными в консоль и очисти
 // значения полей формы методом reset.
+// https://youtu.be/iJq-NwbZL84?t=1669
 
 const form = document.querySelector('.login-form');
 const email = form.querySelector('input[type = "email"]');
 const password = form.querySelector('input[type = "password"]');
-const btn = document.querySelector('button[type = "submit"]');
+const btnSubmit = document.querySelector('button[type = "submit"]');
 
-// console.dir(formEl);
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  //   отмена перезагрузки браузера после отправки формы
+  event.preventDefault();
+  console.log(event.currentTarget);
+  console.log('123');
+}
+
+console.dir(form);
 console.log(email);
 console.log(password);
-console.log(btn);
+console.log(btnSubmit);
