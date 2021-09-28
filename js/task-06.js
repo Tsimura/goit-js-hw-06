ab6f4989b52f2b51b80c0b32787452141d74d27e
Yurii Tsimura <chappa2828@gmail.com>
2021-09-28 14:54:16 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const inputEl = document.querySelector('#validation-input');
 
 inputEl.addEventListener('focus', onInputFocus);
 inputEl.addEventListener('blur', onInputBlur);
-inputEl.addEventListener('input', onInput);
+inputEl.addEventListener('change', onInput);
 
 function onInputFocus() {
   console.log('input в стані "focus"');
@@ -13,7 +13,9 @@ function onInputBlur() {
 }
 
 function onInput(event) {
-  if (this.value.length == this.getAttribute('data-length')) {
+  // this.style.outline = '0';
+  // this.style.outlineOffset = '0';
+  if (event.currentTarget.value.length == this.getAttribute('data-length')) {
     this.classList.add('valid');
     this.classList.remove('invalid');
     console.log('Вірна умова! Продовжуємо.');
@@ -22,4 +24,5 @@ function onInput(event) {
     this.classList.remove('valid');
     console.log('Не вірна умова! кількість символів 6.');
   }
+  // console.log(event.currentTarget.value);
 }
