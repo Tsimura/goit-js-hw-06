9267e2811eb5c84f318e233538dbf559a410f97e
Yurii Tsimura <chappa2828@gmail.com>
2021-09-29 11:36:51 +0300

    task - 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnEl = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color');
+
+console.log(btnEl);
+console.log(colorValue);
+
+btnEl.addEventListener('click', function () {
+  document.body.style.background = `${getRandomHexColor()}`;
+  colorValue.textContent = `${getRandomHexColor()}`;
+});
