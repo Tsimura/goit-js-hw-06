4538c012e1df79e62a415019f81e831243895bcc
Yurii Tsimura <chappa2828@gmail.com>
2021-09-29 10:30:20 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,35 +1,41 @@
-// Обработка отправки формы form.login-form должна быть по
-// событию submit.
-
-// При отправке формы страница не должна перезагружаться.
-
-// Если в форме есть незаполненные поля, выводи alert с
-// предупреждением о том, что все поля должны быть заполнены.
-
-// Если пользователь заполнил все поля и отправил форму,
-// собери значения полей в обьект, где имя поля будет именем свойства,
-// а значение поля - значением свойства.Для доступа к элементам
-// формы используй свойство elements.
-
-// Выведи обьект с введенными данными в консоль и очисти
-// значения полей формы методом reset.
 // https://youtu.be/iJq-NwbZL84?t=1669
 
 const form = document.querySelector('.login-form');
 const email = form.querySelector('input[type = "email"]');
 const password = form.querySelector('input[type = "password"]');
-const btnSubmit = document.querySelector('button[type = "submit"]');
+const btnSubmit = form.querySelector('button[type = "submit"]');
 
 form.addEventListener('submit', onFormSubmit);
 
+// Варіант 1: Збирає значення і виводить в консоль
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   const formData = new FormData(event.currentTarget);
+//   formData.forEach((value, name) => {
+//     console.log('name: ', name);
+//     console.log('value: ', value);
+//   });
+// }
+
+//Варіант 2: при необхідності звернутись до значення
+
 function onFormSubmit(event) {
-  //   отмена перезагрузки браузера после отправки формы
+  // отмена перезагрузки браузера после отправки формы:
   event.preventDefault();
-  console.log(event.currentTarget);
-  console.log('123');
+
+  // console.dir(event.currentTarget.elements);
+  const formElement = event.currentTarget.elements;
+  const email = formElement.email.value;
+  const password = formElement.password.value;
+
+  // console.log('email: ', email, 'password: ', password);
+
+  const formData = { email, password };
+  console.log(formData);
 }
 
-console.dir(form);
-console.log(email);
-console.log(password);
-console.log(btnSubmit);
+// console.dir(form);
+// console.log(email);
+// console.log(password);
+// console.log(btnSubmit);
