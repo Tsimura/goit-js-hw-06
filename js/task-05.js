666109eb286d50b02ce75c2d9ca98db0d943dca9
Tsimura <chappa2828@gmail.com>
2021-10-02 20:30:44 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,12 @@
 const inputEl = document.querySelector('input#name-input');
 const titleEl = document.querySelector('span#name-output');
 
-// titleEl.textContent = 'Anonymous';
-
 inputEl.addEventListener('input', onFormSubmit);
 
 function onFormSubmit(event) {
-  titleEl.textContent = event.currentTarget.value;
+  if (event.currentTarget.value != '') {
+    titleEl.textContent = event.currentTarget.value;
+  } else titleEl.textContent = 'Anonymous';
 }
 
 // console.log(inputEl);
